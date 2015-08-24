df2fc640a4293b8965f4aad069adfc1d7c332a05
Nicolas Luck <nicolas@lucksus.eu>
2015-08-25 00:28:37 +0200

    Survey language switch and button update

--- js/united-earth.js
+++ js/united-earth.js
@@ -1,42 +1,77 @@
 $( document ).ready(function() {
+  var resetButtons = function(){
+    $('#about .btn-group button').removeClass('btn-success');
+  }
+
   $('#en-button').click(function(){
-    if($('#en').hasClass('in'))
-      $('#en').collapse('hide');
-    else
-      $('#en').collapse('show');
     $('#de').collapse('hide');
     $('#es').collapse('hide');
     $('#fr').collapse('hide');
+    resetButtons();
+    $('#en-button').addClass('btn-success');
+    if($('#en').hasClass('in')){
+      $('#en').collapse('hide');
+      resetButtons();
+    }
+    else
+      $('#en').collapse('show');
   });
 
   $('#fr-button').click(function(){
-    if($('#fr').hasClass('in'))
-      $('#fr').collapse('hide');
-    else
-      $('#fr').collapse('show');
     $('#en').collapse('hide');
     $('#de').collapse('hide');
     $('#es').collapse('hide');
+    resetButtons();
+    $('#fr-button').addClass('btn-success');
+    if($('#fr').hasClass('in')){
+      resetButtons();
+      $('#fr').collapse('hide');
+    }
+    else
+      $('#fr').collapse('show');
   });
 
   $('#es-button').click(function(){
-    if($('#es').hasClass('in'))
-      $('#es').collapse('hide');
-    else
-      $('#es').collapse('show');
     $('#es').collapse('show');
     $('#en').collapse('hide');
     $('#de').collapse('hide');
     $('#fr').collapse('hide');
+    resetButtons();
+    $('#es-button').addClass('btn-success');
+    if($('#es').hasClass('in')){
+      resetButtons();
+      $('#es').collapse('hide');
+    }
+    else
+      $('#es').collapse('show');
   });
 
   $('#de-button').click(function(){
-    if($('#de').hasClass('in'))
-      $('#de').collapse('hide');
-    else
-      $('#de').collapse('show');
     $('#en').collapse('hide');
     $('#es').collapse('hide');
     $('#fr').collapse('hide');
+    resetButtons();
+    $('#de-button').addClass('btn-success');
+    if($('#de').hasClass('in')){
+      resetButtons();
+      $('#de').collapse('hide');
+    }
+    else
+      $('#de').collapse('show');
+  });
+
+  var resetSurveyButtons = function(){
+    $('#survey .translation-buttons button').removeClass('btn-success');
+  }
+
+  $('#en-survey-button').click(function(){
+    $('#survey-iframe').prop('src', 'https://docs.google.com/forms/d/1YbQ1RjpsOifaz5IMOWnmHNQ9lVXRXTlACC0CmkiqCUQ/viewform?embedded=true');
+    resetSurveyButtons();
+    $('#en-survey-button').addClass('btn-success');
+  });
+  $('#de-survey-button').click(function(){
+    $('#survey-iframe').prop('src', 'https://docs.google.com/forms/d/1GwT-nSoQac7Pg1Xa30wi-rSnJtfhAUnnHUGLZ1W024s/viewform?embedded=true');
+    resetSurveyButtons();
+    $('#de-survey-button').addClass('btn-success');
   });
 });
